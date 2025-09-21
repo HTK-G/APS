8dbcdf669d970487b9e521a3637b4bf52dd208d1
HTK-G <hg2655@nyu.edu>
2025-09-21 19:35:13 -0400

    Added ArcheologicalPuzzle

--- Main.java
+++ Main.java
@@ -1,99 +1,81 @@
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
+// This program is for submission's only
+// Content subject to change at any moment
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
+        // Input section
         Scanner input = new Scanner(System.in);
-        long a = input.nextLong();
-        long b = input.nextLong();
+        char[] instruction = input.next().toUpperCase().toCharArray();
+        int n = input.nextInt();
+        String data = input.next();
+        // System.out.println("instruction: " + instruction + " n: " + n + " Data: " + data);
         input.close();
 
-        // Edge check:
-        if (a >= b) {
-            System.out.println(-1);
-            return;
-        }
+        Deque<Integer> puzzle = new ArrayDeque<>();
 
-        int bound = (int) Math.sqrt(b) + 1;
-        ArrayList<Integer> basePrimes = sieve(bound);
-
-        // Calculation steps:
-        boolean[] primes = new boolean[(int) (b - a + 1)]; // this track only [a, b]
-        Arrays.fill(primes, true);
-
-        for (int p : basePrimes) {
-            // find the first multiple of p in [a, b]
-            long start = Math.max((long) p * p, ((a + p - 1) / p) * (long) p);
-            for (long j = start; j <= b; j += p) {
-                primes[(int) (j - a)] = false;
+        String trimmed = data.substring(1, data.length() - 1); // remove [ ]
+        if (!trimmed.isEmpty()) {
+            for (String numStr : trimmed.split(",")) {
+                puzzle.add(Integer.parseInt(numStr));
             }
         }
-        if (a == 1) {
-            primes[0] = false;
-        }
+        // Initiate the deque
+        int i = 0;
+        boolean fifo = true;
+        while (i < instruction.length) {
 
-        int prev = -1;
-        int[] minPair = new int[]{-1, -1};
-        int[] maxPair = new int[]{-1, -1};
-
-        for (int i = 0; i < b - a + 1; i++) {
-            if (primes[i]) {
-                int current = (int) (i + a);
-                if (prev != -1) {
-                    int gap = current - prev;
-                    // update min pair
-                    if (minPair[0] == -1 || gap < (minPair[1] - minPair[0])) {
-                        minPair[0] = prev;
-                        minPair[1] = current;
-                    }
-                    // update max pair
-                    if (maxPair[0] == -1 || gap > (maxPair[1] - maxPair[0])) {
-                        maxPair[0] = prev;
-                        maxPair[1] = current;
-                    }
+            if (instruction[i] == 'R') {
+                int count = 0;
+                while (i < instruction.length && instruction[i] == 'R') {
+                    count++;
+                    i++;
                 }
-                prev = current;
+                if (count % 2 == 1) {
+                    fifo = !fifo;
+                }
+            } else { // 'D'
+                if (puzzle.isEmpty()) {
+                    System.out.println("error");
+                    return;
+                }
+
+                if (fifo) { // use poll
+                    puzzle.removeFirst();
+                } else {
+                    puzzle.removeLast();
+                }
+                i++;
             }
         }
 
-        // output section
-        if (minPair[0] == -1) {
-            System.out.println(-1);
+        if (puzzle.isEmpty()) {
+            System.out.println("[]");
         } else {
-            System.out.println(minPair[0] + " " + minPair[1] + " " + maxPair[0] + " " + maxPair[1]);
-        }
-    }
+            StringBuilder sb = new StringBuilder();
+            sb.append('[');
 
-    public static ArrayList<Integer> sieve(int bound) {
+            if (fifo) {
 
-        boolean[] prime = new boolean[bound];
+                while (!puzzle.isEmpty()) {
 
-        for (int i = 2; i < prime.length; i++) {
-            prime[i] = true;
-        }
+                    sb.append(puzzle.removeFirst());
+                    sb.append(',');
 
-        int limit = (int) Math.sqrt(prime.length);
-        for (int i = 2; i < limit; i++) {
+                }
+            } else {
+                while (!puzzle.isEmpty()) {
 
-            if (prime[i]) {
-
-                for (int j = i * i; j < prime.length; j += i) {
-
-                    prime[j] = false;
+                    sb.append(puzzle.removeLast());
+                    sb.append(',');
                 }
             }
+            sb.deleteCharAt(sb.length() - 1);
+            sb.append(']');
+            System.out.println(sb.toString());
         }
 
-        ArrayList<Integer> basePrimes = new ArrayList<>();
-
-        for (int i = 0; i < prime.length; i++) {
-            if (prime[i]) {
-                basePrimes.add(i);
-            }
-        }
-        return basePrimes;
     }
 }
--- ProblemSet3/ArcheologicalPuzzle.java
+++ ProblemSet3/ArcheologicalPuzzle.java
@@ -1,12 +1,81 @@
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.Scanner;
 
 public class ArcheologicalPuzzle {
 
     public static void main(String[] args) {
-
+        // Input section
         Scanner input = new Scanner(System.in);
-
+        char[] instruction = input.next().toUpperCase().toCharArray();
+        int n = input.nextInt();
+        String data = input.next();
+        // System.out.println("instruction: " + instruction + " n: " + n + " Data: " + data);
         input.close();
+
+        Deque<Integer> puzzle = new ArrayDeque<>();
+
+        String trimmed = data.substring(1, data.length() - 1); // remove [ ]
+        if (!trimmed.isEmpty()) {
+            for (String numStr : trimmed.split(",")) {
+                puzzle.add(Integer.parseInt(numStr));
+            }
+        }
+        // Initiate the deque
+        int i = 0;
+        boolean fifo = true;
+        while (i < instruction.length) {
+
+            if (instruction[i] == 'R') {
+                int count = 0;
+                while (i < instruction.length && instruction[i] == 'R') {
+                    count++;
+                    i++;
+                }
+                if (count % 2 == 1) {
+                    fifo = !fifo;
+                }
+            } else { // 'D'
+                if (puzzle.isEmpty()) {
+                    System.out.println("error");
+                    return;
+                }
+
+                if (fifo) { // use poll
+                    puzzle.removeFirst();
+                } else {
+                    puzzle.removeLast();
+                }
+                i++;
+            }
+        }
+
+        if (puzzle.isEmpty()) {
+            System.out.println("[]");
+        } else {
+            StringBuilder sb = new StringBuilder();
+            sb.append('[');
+
+            if (fifo) {
+
+                while (!puzzle.isEmpty()) {
+
+                    sb.append(puzzle.removeFirst());
+                    sb.append(',');
+
+                }
+            } else {
+                while (!puzzle.isEmpty()) {
+
+                    sb.append(puzzle.removeLast());
+                    sb.append(',');
+                }
+            }
+            sb.deleteCharAt(sb.length() - 1);
+            sb.append(']');
+            System.out.println(sb.toString());
+        }
+
     }
 }
