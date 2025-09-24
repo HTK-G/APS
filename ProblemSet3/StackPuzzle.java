afb8c0a9cc431865751db5a69c0e29e6724f4804
Harrison Gao <131316767+HTK-G@users.noreply.github.com>
2025-09-24 00:57:08 -0400

    Update the Stack Puzzle Question

--- ProblemSet3/StackPuzzle.java
+++ ProblemSet3/StackPuzzle.java
@@ -1,13 +1,103 @@
 
+import java.lang.reflect.Array;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class StackPuzzle {
 
     public static void main(String[] args) {
+        // Input section
         Scanner input = new Scanner(System.in);
+
+        char[] before = input.next().toCharArray();
+        char[] after = input.next().toCharArray();
+
+        StringBuilder cur = new StringBuilder(); // this can also be a "stack"
+
+        if (before.length != after.length) {
+            System.out.println("[");
+            System.out.println(']');
+            return;
+        }
+
+        HashMap<Character, Integer> freq = new HashMap<>();
+        for (Character c : before) {
+
+            freq.put(c, freq.getOrDefault(c, 0) + 1);
+        }
+        for (Character c : after) {
+            if (freq.containsKey(c) && freq.get(c) != 0) {
+                freq.put(c, freq.get(c) - 1);
+            } else {
+                System.out.println("[");
+                System.out.println(']');
+                return;
+            }
+        }
+
+        // Calculation Steps
+        ArrayList<ArrayList<Character>> paths = new ArrayList<>();
+        dfs(before, after, 0, 0, paths, new ArrayList<>(), new ArrayDeque<>());
+
+        
+        // Output section
+        System.out.println("[");
+        for (ArrayList<Character> path : paths) {
+            for (int k = 0; k < path.size(); k++) {
+                System.out.print(path.get(k));
+                if (k < path.size() - 1) {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println("]");
 
         input.close();
 
+    }
+
+    // i is the index for "before" and j is the index for "after"
+    // At this step, it is guaranteed that len(before) == len(after)
+    public static void dfs(
+            char[] before, char[] after, int i, int j,
+            ArrayList<ArrayList<Character>> paths, ArrayList<Character> path, Deque<Character> stack) {
+
+        // This recursive method is going deeeep
+        // base case:
+        if (i == before.length && j == after.length) {
+            paths.add(new ArrayList<>(path));
+            return;
+        }
+
+        // We do 'i' operation first for lexicographical purpose
+        if (i < before.length) {
+
+            stack.push(before[i]);
+            i++;
+            path.add('i');
+            dfs(before, after, i, j, paths, path, stack);
+            stack.pop();
+            path.remove(path.size() - 1);
+            i--;
+
+        }
+
+        // Then we check for "o" operation
+        if (!stack.isEmpty() && stack.peek() == after[j]) {
+
+            char temp = stack.pop();
+            j++;
+            path.add('o');
+            dfs(before, after, i, j, paths, path, stack);
+            stack.push(temp);
+            path.remove(path.size() - 1);
+            j--;
+        }
     }
 
 }
