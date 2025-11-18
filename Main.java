d35767922f396f883e1f7b1a9fde40799349d9f3
HTK-G <hg2655@nyu.edu>
2025-11-18 00:25:57 -0500

    Finished <NoChange>

--- Main.java
+++ Main.java
@@ -2,98 +2,54 @@
 // This program is for submission's only
 // Content subject to change at any moment
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        // Input section
+        // Input section:
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        int price = Integer.parseInt(br.readLine());
 
-        int A = Integer.parseInt(st.nextToken());
-        int B = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(br.readLine());
 
-        // Base cases check:
-        // if no 1 or only 1, then output is 1
-        if (A == 0 || A == 1) {
+        int[] coins = new int[N];
 
-            System.out.println(A);
-            return;
+        int total = 0;
+
+        for (int i = 0; i < N; i++) {
+            coins[i] = Integer.parseInt(br.readLine());
+            total += coins[i];
         }
 
-        // if no 0? then every digit is 1
-        if (B == 0) {
+        // ith position in dp array stores the minimum coin needed
+        int[] dp = new int[total + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
 
-            long ans = 0L;
+        for (int coin : coins) {
 
-            ans |= (1L << A);
-            ans--;
+            for (int s = total; s >= coin; s--) {
 
-            System.out.println(ans);
+                if (dp[s - coin] != Integer.MAX_VALUE) {
 
-            return;
-        }
-
-        // here from our observation, a string can have at most
-        // 2 * B '01' or '10's
-        // To illustrate this, see: 101101101, depending on how
-        // many 0s we have, we can form at most B 101 pairs, so
-        // therefore at most 2B 1s are paired with 0
-        // The rest of 1s, we just put them in the right most
-        // section to keep the number as small as possible
-        // Structure: _1__0__11__0__11__0__1__
-        // the first and last can has at most one 1, since 101.
-        // int ones = Math.max(A, (2 * B));
-        int remainOnes = A;
-
-        int[] bits = new int[B + 1];
-
-        // think of from right to left
-        for (int i = 0; i <= B; i++) {
-
-            int maxOnes = 2;
-
-            // if we are at both ends
-            // the first and last can has at most one 1, since 101.
-            if (i == 0 || i == B) {
-
-                maxOnes = 1;
+                    dp[s] = Math.min(dp[s], dp[s - coin] + 1);
+                }
             }
-
-            while (maxOnes != 0 && remainOnes != 0) {
-
-                bits[i]++;
-                remainOnes--;
-                maxOnes--;
-            }
-
         }
 
-        // The rest 1: _1__0__11__0__11__0__111111__
-        if (remainOnes != 0) {
+        // Output
+        for (int i = price; i <= total; i++) {
 
-            bits[0] += remainOnes;
-            remainOnes = 0;
+            if (dp[i] != Integer.MAX_VALUE) {
+
+                System.out.println(i + " " + dp[i]);
+                return;
+            }
         }
 
-        long ans = 0L;
-
-        for (int i = B; i >= 0; i--) {
-
-            while (bits[i] != 0) {
-
-                ans = ((ans << 1L) | 1L);
-                bits[i]--;
-            }
-            if (i > 0) {
-                ans = (ans << 1L);
-            }
-
-        }
-
-        System.out.println(ans);
+        System.out.println(-1);
     }
 }
--- ProblemSetA/NoChange.java
+++ ProblemSetA/NoChange.java
@@ -1,3 +1,55 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
 public class NoChange {
-    
+
+    public static void main(String[] args) throws IOException {
+
+        // Input section:
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int price = Integer.parseInt(br.readLine());
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] coins = new int[N];
+
+        int total = 0;
+
+        for (int i = 0; i < N; i++) {
+            coins[i] = Integer.parseInt(br.readLine());
+            total += coins[i];
+        }
+
+        // ith position in dp array stores the minimum coin needed
+        int[] dp = new int[total + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+
+        for (int coin : coins) {
+
+            for (int s = total; s >= coin; s--) {
+
+                if (dp[s - coin] != Integer.MAX_VALUE) {
+
+                    dp[s] = Math.min(dp[s], dp[s - coin] + 1);
+                }
+            }
+        }
+
+        // Output
+        for (int i = price; i <= total; i++) {
+
+            if (dp[i] != Integer.MAX_VALUE) {
+
+                System.out.println(i + " " + dp[i]);
+                return;
+            }
+        }
+
+        System.out.println(-1);
+    }
 }
